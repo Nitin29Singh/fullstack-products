e1ab855e972bb77eaf4e4b260852bcf839717cb9
Nitin29Singh <nitin2004singhh29@gmail.com>
2025-10-30 09:48:07 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ProductList from "./components/ProductList";
+
+function App() {
+  return (
+    <div style={{ backgroundColor: "#222", height: "100vh", color: "white" }}>
+      <ProductList />
+    </div>
+  );
+}
+
+export default App;
